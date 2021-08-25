f1beb4117f9453138b5cd7738f40b2096a1c92fa
Thales <thaleshpied@gmail.com>
2021-08-24 21:23:59 -0300

    Dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -6,11 +6,10 @@
 <!-- Begin Page Content -->
         <div class="container-fluid">
 
-          <!-- Page Heading - GERADOR DE RELATÓRIO 
+          <!-- Page Heading - GERADOR DE RELATÓRIO  -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-          </div>-->
+            <h1 class="h6 mb-0 text-gray-800">Dashboard</h1>
+          </div>
 
           <!-- Content Row -->
           <div class="row">
@@ -55,10 +54,10 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Pevisão próximo mês</div>
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Custo Previsto próximo mês</div>
                       <div class="row no-gutters align-items-center">
                         <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">0%</div>
+                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">R$</div>
                         </div>
                         <div class="col">
                           <div class="progress progress-sm mr-2">
@@ -75,7 +74,7 @@
               </div>
             </div> -->
 
-            <!-- Pending Requests Card Example 
+            <!-- Pending Requests Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
@@ -91,7 +90,7 @@
                 </div>
               </div>
             </div>
-          </div> -->
+          </div> 
 
           <!-- Content Row -->
 
