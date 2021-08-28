3d63954793b5177cec3572c8688fd5ba848e3111
Thales <thaleshpied@gmail.com>
2021-08-27 22:23:13 -0300

    Evolução na visualização dos fornecedores
    Trabalhando na parte visual de edição e exclusão de fornecedores

--- application/views/fornecedores.php
+++ application/views/fornecedores.php
@@ -1,63 +1,58 @@
-   <!-- <div class="col-12">
-          <a href="{{url('/gerenciamento/categoria/create')}}" class="btn btn-success float-right"><i class="fas fa-plus"></i><span>Novo Fornecedor</span></a>
-        </div>-->
+<div class="row">
+<div class="col-12">
+          <a href="{{url('/gerenciamento/categoria/create')}}" class="btn btn-success ml-2"><i class="fas fa-plus"></i><span> Novo Fornecedor</span></a>
+        </div>
+
+  
+</div>
 <div id="accordion">
   <div class="card-header" id="headingOne">
     <div class="card row">   
-      <div class="col-8">
-        <h5 class="mb-0">
-        <a data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Fornecedor <small> Dados</small>
-        </a>
-      </h5>
+      <div class="col-12">
 
       <?php foreach ($fornecedores as $p): ?>
                             
-            <div class="media text-muted pt-3 border-bottom">
+            <div class="media text-muted pt-3">
                 <p class="media-body pb-3 mb-0 small lh-125 text-body">                              
-                Nome: <?php echo $p['nome']; ?></p><br>
-                Endereço: <?php echo $p['endereço']; ?>
+                Nome: <?php echo $p['nome']; ?> - Telefone: <?php echo $p['telefone']; ?> 
+                <br>
+                Endereço: <?php echo $p['endereço']; ?>                  
+                </p>
             </div>                                           
-      <?php endforeach ?>                    
+      
+      <div>
+        
+              <button class="btn btn-warning mb-1 mr-5 float-right fonrecedoresopcoes" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><i class="fas fa-pencil-alt"></i></button>
+              <button class="btn btn-danger mb-1 float-right fonrecedoresopcoes"><i class="fas fa-times"></i></button>
+      </div>
+
+
+      <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+        <div class="card-body">
+            <h6>Editor de cadastro:</h6>
+            <div class="input-div pt-1" id="input-valor">
+              <input class="form-control" id="nome" name="nome" type="text" widht="60px" placeholder="Nome: <?php echo $p['nome']; ?>" required />
+            </div>
+            <div class="input-div pt-1" id="input-valor">
+              <input class="form-control" id="telefone" name="telefone" type="text" widht="60px" placeholder="Telefone: <?php echo $p['telefone']; ?>" required />
+            </div>
+            <div class="input-div pt-1" id="input-valor">
+              <input class="form-control" id="endereço" name="endereço" type="text" widht="60px" placeholder="Endereço: <?php echo $p['endereço']; ?>" required />
+            </div>
+            <div class="input-div pt-1" id="input-valor">
+              <button class="btn btn-outline-info mb-3" type="submit" name="Alterar" id="botao">Salvar</button>
+            </div>
+        </div>
+      </div>
+
+      
+        
+    <?php endforeach ?>                    
+      </div>   
+    </div>
+  </div>
+</div>
                   
 
 
         
-      </div>   
-      <div class="col-4">
-        
-              <button class="btn btn-warning"><i class="fas fa-pencil-alt"></i></button>
-              <button class="btn btn-danger"><i class="fas fa-times"></i></button>
-      </div> 
-      <h6 class="mb-0 text-center bg-info bg-gradient">
-        <a data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Detalhes
-        </a>
-      </h6>  
-    </div>
-
-      <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-        <div class="card-body">
-            Detalhes
-        </div>
-      </div>
-  </div>
-
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h5 class="mb-0">
-        <a data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Fornecedor <small> Dados</small>
-        </a>
-      </h5>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-      <div class="card-body">
-        <div class="row ">
-
-        aqi
-      </div>
-      </div>
-    </div>
-  </div>  
-</div>
