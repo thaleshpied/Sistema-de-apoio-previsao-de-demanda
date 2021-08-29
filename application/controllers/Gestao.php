04ba69154826588f2fc4999f28ab129d3c524e92
Thales <thaleshpied@gmail.com>
2021-08-29 19:44:08 -0300

    Controller BI

--- application/controllers/Gestao.php
+++ application/controllers/Gestao.php
@@ -9,6 +9,7 @@ class Gestao extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->model('Produto_model');
+		$this->load->model('Fornecedores_model');
 		$this->load->model('Consumo_produto_model');
 		$this->load->helper('funcoes_helper');		
 	}
@@ -17,16 +18,21 @@ class Gestao extends CI_Controller {
 	public function bi()
 	{	
 		$dados['title'] = "Business Intelligence";
+		
 		$produtos = $this->Produto_model->get_all_produto();
 		$dados['produtos'] = $produtos;
+
 		$quantidadetotal = $this->Produto_model->cont_all_produto();
 		$dados['quantidadetotal'] = $quantidadetotal;
+
 		$consumototal = $this->Consumo_produto_model->get_all_consumo_produto();
 		$dados['consumototal'] = $consumototal;
 
 		$valortotal = 0;
 		$dados['valortotal'] = $valortotal;
 
+		$qtdtotalfornecedores = $this->Fornecedores_model->cont_all_fornecedores();
+		$dados['qtdtotalfornecedores'] = $qtdtotalfornecedores;
 
 		$this->load->view('components/head.php', $dados);
 		$this->load->view('components/nav.php', $dados);
@@ -35,133 +41,4 @@ class Gestao extends CI_Controller {
 	}
 
 
-
-
-
-
-
-
-
-
-
-
-	public function cadastrar()
-	{	
-		$dados['title'] = "Cadastrar Produtos";
-		$produtos = $this->Produto_model->get_all_produto();
-		$dados['produtos'] = $produtos;
-		$quantidadetotal = $this->Produto_model->cont_all_produto();
-		$dados['quantidadetotal'] = $quantidadetotal;
-
-		$valortotal = 0;
-		$dados['valortotal'] = $valortotal;	
-
-		$this->load->view('components/head.php', $dados);
-		$this->load->view('components/nav.php', $dados);
-		$this->load->view('dashboard.php', $dados);
-		$this->load->view('submenuprodutos.php');
-		$this->load->view('cadastrar_produtos.php', $dados);
-		$this->load->view('components/foother.php', $dados);
-	}
-
-	public function editar()
-	{	
-		$dados['title'] = "Editar Produtos";
-		$produtos = $this->Produto_model->get_all_produto();
-		$dados['produtos'] = $produtos;
-		$quantidadetotal = $this->Produto_model->cont_all_produto();
-		$dados['quantidadetotal'] = $quantidadetotal;
-		$valortotal = 0;
-		$dados['valortotal'] = $valortotal;	
-
-		$this->load->view('components/head.php', $dados);
-		$this->load->view('components/nav.php', $dados);
-		$this->load->view('dashboard.php', $dados);
-		$this->load->view('submenuprodutos.php');
-		$this->load->view('editar_produtos.php', $dados);
-		$this->load->view('components/foother.php', $dados);
-	}
-
-	public function consumo()
-	{	
-		$dados['title'] = "Consumo de Produtos";
-		$produtos = $this->Produto_model->get_all_produto();
-		$dados['produtos'] = $produtos;
-		$quantidadetotal = $this->Produto_model->cont_all_produto();
-		$dados['quantidadetotal'] = $quantidadetotal;
-
-		$consumototal = $this->Consumo_produto_model->get_all_consumo_produto();
-		$dados['consumototal'] = $consumototal;
-
-		$valortotal = 0;
-		$dados['valortotal'] = $valortotal;	
-
-		$this->load->view('components/head.php', $dados);
-		$this->load->view('components/nav.php', $dados);
-		$this->load->view('dashboard.php', $dados);
-		$this->load->view('submenuprodutos.php');
-		$this->load->view('consumo_produtos.php', $dados);
-		$this->load->view('components/foother.php', $dados);
-	}
-
-	public function add()
-	{   
-		if(isset($_POST) && count($_POST) > 0)     
-		{   
-			$params = array(
-				'nome' => $this->input->post('nome'),
-				'preco' => floatval($this->input->post('preco')) * 100,
-				'descricao' => $this->input->post('descricao'),
-				'quantidade' => $this->input->post('quantidade'),
-				'imagem' => $this->input->post('imagem'),
-				'tipo' => $this->input->post('tipo'),
-				'estoquemax' => $this->input->post('estoquemax'),
-			);
-			
-			$produto_id = $this->Produto_model->add_produto($params);
-			
-			$mensagem = 'Produto cadastrado com sucesso!';
-			echo json_encode($mensagem);
-		}
-		else
-		{            
-			$mensagem = 'Ocorreu um erro ao enviar os dados!';
-			echo json_encode($mensagem);
-		}
-	}
-	public function edit()
-	{   
-		$idProduto = $this->input->post('idProduto');
-		// check if the produto exists before trying to edit it
-		$data['produto'] = $this->Produto_model->get_produto($idProduto);
-		
-		if(isset($data['produto']['idProduto']))
-		{
-			if(isset($_POST) && count($_POST) > 0)     
-			{   
-				$params = array(
-					'nome' => $this->input->post('nome'),
-					'preco' => floatval($this->input->post('preco')) * 100,
-					'descricao' => $this->input->post('descricao'),
-					'quantidade' => $this->input->post('quantidade'),
-				);
-
-				$this->Produto_model->update_produto($idProduto,$params);  
-
-				$mensagem = 'Produto alterado com sucesso!';
-				echo json_encode($mensagem);
-				return true;
-			}
-			else
-			{
-				$mensagem = 'Ocorreu um erro ao enviar os dados!';
-				echo json_encode($mensagem);
-				return true;
-			}
-		}
-		else
-			$mensagem = 'Selecione um produto para editar!';
-			echo json_encode($mensagem);
-			return true;
-	}
 }
