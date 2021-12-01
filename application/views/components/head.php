224f664b8454179c022a3d3b8cdcc84ddd94fe56
Thales <thaleshpied@gmail.com>
2021-11-30 21:31:49 -0300

    Ajuste Head

--- application/views/components/head.php
+++ application/views/components/head.php
@@ -6,8 +6,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <meta name="description" content="Sistema de Apoio A previsão de demanda">
+  <meta name="author" content="Thales Henrique">
   <meta name="mobile-web-app-capable" content="yes">
 
   <!-- Icone site (Fav Icon)-->
